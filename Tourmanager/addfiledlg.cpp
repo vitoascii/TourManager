f99f3dec3b992c13e80fe25a337f9d66d21c4757
vitoascii <1583308688@qq.com>
2015-04-05 19:55:56 +0800

    Signed-off-by: vitoascii <1583308688@qq.com>

--- Tourmanager/addfiledlg.cpp
+++ Tourmanager/addfiledlg.cpp
@@ -6,12 +6,32 @@
 #include <QString>
 #include <QStandardPaths>
 #include <QMessageBox>
+#include <QBuffer>
+#include <QByteArray>
+#include <QGridLayout>
 
 AddFileDlg::AddFileDlg(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AddFileDlg)
 {
     ui->setupUi(this);
+    QGridLayout*layout=new QGridLayout;
+    layout->addWidget(ui->wLblTprz,0,0,3,10);
+    layout->addWidget(ui->wLetTitle,3,0,2,30);
+    layout->addWidget(ui->wDetDate,5,0,1,7);
+    layout->addWidget(ui->wLblPlace,5,7,1,3);
+    layout->addWidget(ui->wLetPlace,5,10,1,10);
+    layout->addWidget(ui->wTetText,6,0,25,30);
+    layout->addWidget(ui->wBtnAddImage,31,0,1,5);
+    layout->addWidget(ui->wBtnPreviewImage,31,5,1,5);
+    layout->addWidget(ui->wBtnDelete,31,12,1,2);
+    layout->addWidget(ui->wLbl1,31,14,1,1);
+    layout->addWidget(ui->wLetIndexDel,31,15,1,2);
+    layout->addWidget(ui->wLbl2,31,17,1,1);
+    layout->addWidget(ui->wLblImageAmount,31,19,1,8);
+    layout->addWidget(ui->wBtnSubmit,33,0,1,5);
+    layout->addWidget(ui->wBtnCancel,33,8,1,5);
+    setLayout(layout);
 
 }
 
@@ -61,6 +81,7 @@ void AddFileDlg::on_wBtnSubmit_clicked()
          else
              QMessageBox::warning(this,tr("Can't save dairy!"),tr("Failed to create direction!-1"));
     }
+    this->close();
 }
 /*
  * 保存的文件格式为：时间地点/序号/标题.txt+图片文件夹，根目录Mydocument/tourManager_dairy
@@ -108,13 +129,41 @@ void  AddFileDlg::saveFile(QString documentsLocation)
     QString textLocation=saveLocation;
     textLocation.append(diaryTitle.append(".txt"));
     //qDebug()<<diaryText;
-    QFile*file=new QFile(textLocation);
-    if(!file->open(QIODevice::WriteOnly | QIODevice::Text))
+    QFile*file1=new QFile(textLocation);
+    if(!file1->open(QIODevice::WriteOnly | QIODevice::Text))
     {
         QMessageBox::warning(this,tr("Can't save dairy!"),tr("Failed to create file!"));
         return;
     }
-    QTextStream txtOutput(file);
+    QTextStream txtOutput(file1);
     txtOutput<<diaryText<<endl;
-    file->close();
+    file1->close();
+
+    //保存图片
+    if(!saveDir->exists(saveLocation.append("image/")))
+    {
+        qDebug()<<saveLocation;
+        if(!saveDir->mkdir(saveLocation))
+            QMessageBox::warning(this,tr("Can't save dairy!"),tr("Failed to create iamge direction!"));
+    }
+    for(int i=0;i<imageFileNames.size();i++)
+    {
+        tempLocation=saveLocation;
+        QImage*image=new QImage(imageFileNames[i]);
+        QFile*file2=new QFile(tempLocation.append(tr("%1.jpg").arg(i+1)));
+        qDebug()<<tempLocation;
+        if (!file2->open(QIODevice::ReadWrite))
+        {
+            break;
+        }
+        QByteArray ba;
+        QBuffer buffer(&ba);
+        buffer.open(QIODevice::WriteOnly);
+        image->save(&buffer, "JPG");//把图片以流方式写入文件缓存流中
+        file2->write(ba);
+        file2->close();
+        buffer.close();
+    }
 }
+
+
